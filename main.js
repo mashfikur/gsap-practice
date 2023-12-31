8705d1d71a2813f859fb3680962591811753268e
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-31 17:43:22 +0600

    modified animation on homepage

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-let title = document.getElementById("title");
+let title = document.getElementsByClassName("title");
 
 // gsap animation codes
 
 let titleTL = gsap.timeline();
-titleTL.pause();
 
-titleTL.to("#title", {
-  scale: 1.5,
+titleTL.to(".title", {
+  x: 500,
   duraiton: 3,
-  backgroundColor: "red",
-  borderRadius: "30px",
-  padding: " 10px 15px",
+  stagger: 0.5,
+  ease: "elastic.inOut(.6)",
 });
 
-title.addEventListener("mouseenter", () => titleTL.play());
-// title.addEventListener("mouseleave", () => title.reverse());
+// title.addEventListener("mouseenter", () => titleTL.play());
+// // title.addEventListener("mouseleave", () => title.reverse());
